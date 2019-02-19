8a21d89a08f5a1b141b7cf03ab3e4a22e1b233db
Vojtěch Kulvait <kulvait@gmail.com>
2019-02-19 14:55:45 +0100

    Fourier series visualization
    Visualization of Fourier series.

---
+++ include/utils/FourierSeriesEvaluator.hpp
@@ -0,0 +1,403 @@
+#pragma once
+
+#include <algorithm>
+#include <mutex>
+#include <vector>
+
+#include "utils/Attenuation4DEvaluatorI.hpp"
+
+namespace CTL::util {
+
+class FourierSeriesEvaluator : public Attenuation4DEvaluatorI
+{
+public:
+    /**Evaluation of the attenuation values based on the Legendre coeficients fiting.
+     *
+     *@param[in] degree Polynomial degree, should be equal to coefficientVolumes.size() - 1 since
+     *the first coeficient represent constant, that is polynomial of zeroth order.
+     *@param[in] coefficientVolumeFiles Files with fitted coefficient volumes of related Legendre
+     *polynomials.
+     *@param[in] intervalStart Start time.
+     *@param[in] intervalEnd End time.
+     */
+    FourierSeriesEvaluator(uint32_t degree,
+                           std::vector<std::string> coefficientVolumeFiles,
+                           float intervalStart,
+                           float intervalEnd);
+
+    /**Destructor of FourierSeriesEvaluator class
+     *
+     */
+    ~FourierSeriesEvaluator();
+
+    /**Function to obtain time discretization.
+     *
+     *This function evaluates the time instants in which other functions of the implementing class
+     *fills the arrays with values in that times.
+     *@param[in] granularity Number of time points to fill in timePoints array with.
+     *@param[out] timePoints Time discretization.
+     */
+    void timeDiscretization(const uint32_t granularity, float* timePoints) override;
+
+    /**Test if the time is discretized evenly.
+     *
+     * This property is required for assembling pseudoinverse matrix.
+     */
+    bool isTimeDiscretizedEvenly() override;
+
+    /**Function to evaluate time series in given point.
+     *
+     *@param[in] x Zero based x coordinate of the volume.
+     *@param[in] y Zero based y coordinate of the volume.
+     *@param[in] z Zero based z coordinate of the volume.
+     *@param[in] granularity Number of time points to fill in aif array with.
+     *@param[out] aif Prealocated array to put the values at particular times.
+     */
+    void timeSeriesIn(const uint16_t x,
+                      const uint16_t y,
+                      const uint16_t z,
+                      const uint32_t granularity,
+                      float* aif) override;
+
+    /**Function to evaluate the value of attenuation at (x,y,z,t).
+     *
+     *@param[in] x Zero based x coordinate of the volume.
+     *@param[in] y Zero based y coordinate of the volume.
+     *@param[in] z Zero based z coordinate of the volume.
+     *@param[in] t Time of evaluation.
+     */
+    float valueAt(const uint16_t x, const uint16_t y, const uint16_t z, const float t) override;
+
+    /**Function to evaluate the value of attenuation for the whole frame (z,t).
+     *
+     *@param[in] z Zero based z coordinate of the volume.
+     *@param[in] t Time of evaluation.
+     *@param[out] val Prealocated array of size dimx*dimy to put the values of the frame at time t
+     *at frame z.
+     */
+    void frameAt(const uint16_t z, const float t, float* val) override;
+
+    /**Function to evaluate time series of frames of attenuation coefficients.
+     *
+     *@param[in] vz Zero based z coordinate of the volume.
+     *@param[in] granularity Number of time points to fill in aif array with.
+     *@param[out] val Prealocated array to put the values at particular times of the size
+     *granularity*dimx*dimy.
+     */
+    void frameTimeSeries(const uint16_t z, const uint32_t granularity, float* val) override;
+
+private:
+    /**Function to evaluate the value of Legendre polynomial without constant at given point
+     *(x,y,z,t).
+     *
+     *@param[in] x Zero based x coordinate of the volume.
+     *@param[in] y Zero based y coordinate of the volume.
+     *@param[in] z Zero based z coordinate of the volume.
+     *@param[in] t Time of evaluation.
+     */
+    float valueAt_withoutOffset(const uint16_t x, const uint16_t y, const uint16_t z, float t);
+
+    /**Function to evaluate the value of Legendre polynomial without constant at given frame
+     *(z,t).
+     *
+     *@param[in] z Zero based z coordinate of the volume.
+     *@param[in] t Time of evaluation.
+     *@param[in] offset Specified offset to subtract and crop at 0.
+     *@param[out] val Prealocated array to put the values at particular times.
+     */
+    void frameAt_customOffset(const uint16_t z, const float t, float* offset, float* val);
+
+    /**Function to evaluate the value of Legendre polynomial without constant at given point
+     *(x,y,z,intervalStart).
+     *
+     *@param[in] x Zero based x coordinate of the volume.
+     *@param[in] y Zero based y coordinate of the volume.
+     *@param[in] z Zero based z coordinate of the volume.
+     */
+    float valueAt_intervalStart(const uint16_t x, const uint16_t y, const uint16_t z);
+
+    /**Function to evaluate the value of Legendre polynomial without constant at given frame
+     *(z,t).
+     *
+     *@param[in] z Zero based z coordinate of the volume.
+     *@param[out] val Prealocated array to put the values at particular times.
+     */
+    void frameAt_intervalStart(const uint16_t z, float* val);
+
+    uint32_t degree;
+    float intervalStart;
+    float intervalEnd;
+    std::vector<std::shared_ptr<io::Frame2DReaderI<float>>> coefficientVolumes;
+    uint16_t dimx, dimy, dimz;
+    std::shared_ptr<util::VectorFunctionI> fourierEvaluator;
+    float* valuesAtStart;
+    float* coefficientsIntervalStart;
+
+    // Global parameters to support faster operations for certain functions.
+    std::mutex globalsAccess;
+    float* coefficientsStoredTime;
+    float storedTime;
+    /** Function updates the coefficientsStoredTime to match given time.
+     *
+     *@param[in] t Time to update.
+     *
+     */
+    void updateCoefficientsStored(const float t);
+    std::vector<std::shared_ptr<io::Frame2DI<float>>> framesStored;
+    uint16_t storedZ;
+    /** Function updates the framesStored to match given z frame.
+     *
+     *@param[in] z Frame to update.
+     *
+     */
+    void updateFramesStored(const uint16_t z);
+};
+
+FourierSeriesEvaluator::FourierSeriesEvaluator(uint32_t degree,
+                                               std::vector<std::string> coefficientVolumeFiles,
+                                               float intervalStart,
+                                               float intervalEnd)
+    : degree(degree)
+    , intervalStart(intervalStart)
+    , intervalEnd(intervalEnd)
+{
+    if(coefficientVolumeFiles.size() != degree)
+    {
+        io::throwerr("Number of files with Fourier coefficients have to be equal to degree, "
+                     "but is %d and degree=%d.",
+                     coefficientVolumeFiles.size(), degree);
+    }
+    if(degree < 2)
+    {
+        std::string ERR = io::xprintf("There must be at least one more function than constant, "
+                                      "menas degree >= 2, but degree =%d.",
+                                      degree);
+        LOGE << ERR;
+        io::throwerr(ERR);
+    }
+    std::shared_ptr<io::Frame2DReaderI<float>> pr;
+    for(std::size_t i = 1; i < degree; i++)
+    {
+        pr = std::make_shared<io::DenFrame2DReader<float>>(coefficientVolumeFiles[i]);
+        coefficientVolumes.push_back(pr);
+    }
+    io::DenFileInfo di(coefficientVolumeFiles[0]);
+    dimx = di.dimx();
+    dimy = di.dimy();
+    dimz = di.dimz();
+
+    for(std::shared_ptr<io::Frame2DReaderI<float>>& f : coefficientVolumes)
+    {
+        if(f->dimx() != dimx || f->dimy() != dimy || f->dimz() != dimz)
+        {
+            io::throwerr("There are incompatible coefficients!");
+        }
+    }
+    fourierEvaluator = std::make_shared<util::FourierSeries>(degree, intervalStart, intervalEnd, 1);
+    coefficientsStoredTime = new float[degree - 1];
+    coefficientsIntervalStart = new float[degree - 1];
+    fourierEvaluator->valuesAt(intervalStart, coefficientsStoredTime);
+    fourierEvaluator->valuesAt(intervalStart, coefficientsIntervalStart);
+    storedTime = intervalStart;
+    storedZ = 0;
+    for(uint32_t i = 1; i < degree; i++)
+    {
+        framesStored.push_back(coefficientVolumes[i]->readFrame(storedZ));
+    }
+    valuesAtStart = new float[dimx * dimy](); // Constructor is not filling this array
+}
+
+FourierSeriesEvaluator::~FourierSeriesEvaluator()
+{
+    if(coefficientsStoredTime != nullptr)
+    {
+        delete[] coefficientsStoredTime;
+    }
+    if(coefficientsIntervalStart != nullptr)
+    {
+        delete[] coefficientsIntervalStart;
+    }
+    if(valuesAtStart != nullptr)
+    {
+        delete[] valuesAtStart;
+    }
+}
+
+void FourierSeriesEvaluator::timeDiscretization(const uint32_t granularity, float* timePoints)
+{
+    double time = intervalStart;
+    double increment = (intervalEnd - intervalStart) / double(granularity - 1);
+    for(uint32_t i = 0; i != granularity; i++)
+    {
+        timePoints[i] = float(time);
+        time += increment;
+    }
+}
+
+bool FourierSeriesEvaluator::isTimeDiscretizedEvenly() { return true; }
+
+void FourierSeriesEvaluator::timeSeriesIn(
+    const uint16_t x, const uint16_t y, const uint16_t z, const uint32_t granularity, float* val)
+{
+    double time = intervalStart;
+    double increment = (intervalEnd - intervalStart) / double(granularity - 1);
+    for(uint32_t i = 0; i != granularity; i++)
+    {
+        val[i] = valueAt(x, y, z, time);
+        time += increment;
+    }
+}
+
+float FourierSeriesEvaluator::valueAt(const uint16_t x,
+                                      const uint16_t y,
+                                      const uint16_t z,
+                                      const float t)
+{
+    float val0 = valueAt_intervalStart(x, y, z);
+    float v = valueAt_withoutOffset(x, y, z, t);
+    return std::max(float(0), v - val0);
+}
+
+void FourierSeriesEvaluator::updateCoefficientsStored(const float t)
+{
+    if(storedTime != t)
+    {
+        fourierEvaluator->valuesAt(t, coefficientsStoredTime);
+        storedTime = t;
+    }
+}
+
+void FourierSeriesEvaluator::updateFramesStored(const uint16_t z)
+{
+
+    if(storedZ != z)
+    {
+        framesStored.clear();
+        for(uint32_t i = 1; i < degree; i++)
+        {
+            framesStored.push_back(coefficientVolumes[i]->readFrame(z));
+        }
+        storedZ = z;
+    }
+}
+
+float FourierSeriesEvaluator::valueAt_intervalStart(const uint16_t x,
+                                                    const uint16_t y,
+                                                    const uint16_t z)
+{
+
+    std::unique_lock<std::mutex> lock(globalsAccess);
+    updateFramesStored(z);
+    float val = 0.0;
+    for(uint32_t i = 1; i < degree; i++)
+    {
+        val += coefficientsIntervalStart[i - 1] * framesStored[i - 1]->get(x, y);
+    }
+    return val;
+}
+
+float FourierSeriesEvaluator::valueAt_withoutOffset(const uint16_t x,
+                                                    const uint16_t y,
+                                                    const uint16_t z,
+                                                    const float t)
+{
+    std::unique_lock<std::mutex> lock(globalsAccess);
+    updateCoefficientsStored(t);
+    updateFramesStored(z);
+    float val = 0.0;
+    for(uint32_t i = 1; i != degree; i++)
+    {
+        val += coefficientsStoredTime[i - 1] * framesStored[i - 1]->get(x, y);
+    }
+    return val;
+}
+
+void FourierSeriesEvaluator::frameAt(const uint16_t z, const float t, float* val)
+{
+    std::unique_lock<std::mutex> lock(globalsAccess);
+    updateFramesStored(z);
+    std::fill_n(valuesAtStart, dimx * dimy, float(0.0));
+    std::fill_n(val, dimx * dimy, float(0.0));
+    // Initialize values of legendre polynomials at the time intervalStart
+    for(int y = 0; y != dimy; y++)
+    {
+        for(int x = 0; x != dimx; x++)
+        {
+            for(uint32_t d = 1; d < degree; d++)
+            {
+                valuesAtStart[y * dimx + x]
+                    += coefficientsIntervalStart[d - 1] * framesStored[d - 1]->get(x, y);
+            }
+        }
+    }
+    updateCoefficientsStored(t);
+    for(int y = 0; y != dimy; y++)
+    {
+        for(int x = 0; x != dimx; x++)
+        {
+            for(uint32_t d = 1; d < degree; d++)
+            {
+                val[y * dimx + x] += coefficientsStoredTime[d - 1] * framesStored[d - 1]->get(x, y);
+            }
+            val[y * dimx + x] = std::max(float(0), val[y * dimx + x] - valuesAtStart[y * dimx + x]);
+        }
+    }
+}
+
+void FourierSeriesEvaluator::frameAt_customOffset(const uint16_t z,
+                                                  const float t,
+                                                  float* offset,
+                                                  float* val)
+{
+    std::fill_n(val, dimx * dimy, float(0.0));
+    std::unique_lock<std::mutex> lock(globalsAccess);
+    updateFramesStored(z);
+    updateCoefficientsStored(t);
+    for(int y = 0; y != dimy; y++)
+    {
+        for(int x = 0; x != dimx; x++)
+        {
+            for(uint32_t d = 1; d < degree; d++)
+            {
+                val[y * dimx + x] += coefficientsStoredTime[d - 1] * framesStored[d - 1]->get(x, y);
+            }
+            val[y * dimx + x] = std::max(float(0), val[y * dimx + x] - offset[y * dimx + x]);
+        }
+    }
+}
+
+void FourierSeriesEvaluator::frameAt_intervalStart(const uint16_t z, float* val)
+{
+
+    std::fill_n(val, dimx * dimy, float(0.0));
+    std::unique_lock<std::mutex> lock(globalsAccess);
+    updateFramesStored(z);
+
+    for(int y = 0; y != dimy; y++)
+    {
+        for(int x = 0; x != dimx; x++)
+        {
+            for(uint32_t d = 1; d < degree; d++)
+            {
+                val[y * dimx + x]
+                    += coefficientsIntervalStart[d - 1] * framesStored[d - 1]->get(x, y);
+            }
+        }
+    }
+}
+
+void FourierSeriesEvaluator::frameTimeSeries(const uint16_t z,
+                                             const uint32_t granularity,
+                                             float* val)
+{
+    double time = intervalStart;
+    double increment = (intervalEnd - intervalStart) / double(granularity - 1);
+    frameAt_intervalStart(z, val);
+    for(uint32_t i = 1; i < granularity; i++)
+    {
+        frameAt_customOffset(z, time, val, &val[i * dimx * dimy]);
+        time += increment;
+    }
+    std::fill_n(val, dimx * dimy, float(0.0)); // At time zero is concentration zero
+}
+} // namespace CTL::util
---
+++ src/perfviz-fourier.cpp
@@ -0,0 +1,198 @@
+// Logging
+#include "PLOG/PlogSetup.h"
+// External libraries
+#include "CLI/CLI.hpp" //Command line parser
+
+//#include "FittingExecutor.hpp"
+#include "AsyncFrame2DWritterI.hpp"
+#include "DEN/DenAsyncFrame2DWritter.hpp"
+#include "DEN/DenFileInfo.hpp"
+#include "DEN/DenFrame2DReader.hpp"
+#include "FUN/FourierSeries.hpp"
+#include "Frame2DReaderI.hpp"
+#include "SVD/TikhonovInverse.hpp"
+#include "stringFormatter.h"
+#include "utils/Attenuation4DEvaluatorI.hpp"
+#include "utils/FourierSeriesEvaluator.hpp"
+#include "utils/TimeSeriesDiscretizer.hpp"
+
+#if DEBUG
+#include "matplotlibcpp.h"
+
+namespace plt = matplotlibcpp;
+#endif
+
+using namespace CTL;
+
+/// Arguments of the main function.
+struct Arguments
+{
+    /// Function to parse function parameters.
+    int parseArguments(int argc, char* argv[]);
+
+    /// Folder to which output data after the linear regression.
+    std::string outputFolder;
+
+    /// Projection files in a DEN format to use for linear regression.
+    std::vector<std::string> fittedCoefficients;
+
+    /// Number of threads
+    uint16_t threads = 0;
+
+    /// Controls the size of the time interval [ms] that should be identified with 0.0.
+    float startTime = 4117, endTime = 56000;
+
+    /// Coordinates of arthery input function
+    uint16_t ifx, ify, ifz;
+
+    /// Granularity of the time is number of time points analyzed by i.e. convolution
+    uint32_t granularity = 100;
+
+    // Length of one second in the units of the domain
+    float secLength = 1000;
+
+    // Vizualize only ttp
+    bool onlyttp = false;
+};
+
+int Arguments::parseArguments(int argc, char* argv[])
+{
+    CLI::App app{ "Visualization of perfusion parameters CT based on Legendre fiting." };
+    app.add_option("-j,--threads", threads,
+                   "Number of extra threads that application can use. Defaults to 0 which means "
+                   "sychronous execution.")
+        ->check(CLI::Range(0, 65535));
+    app.add_option("-i,--start-time", startTime,
+                   "Start of the interval in miliseconds of the support of the functions of time "
+                   "[defaults to 4117, 247*16.6].")
+        ->check(CLI::Range(0.0, 100000.0));
+    app.add_option("-e,--end-time", endTime,
+                   "End of the interval in miliseconds of the support of the functions of time "
+                   "[defaults to 56000, duration of 9 sweeps].")
+        ->check(CLI::Range(0.0, 100000.0));
+    app.add_option("-g,--granularity", granularity,
+                   "Granularity of the time is number of time points to which time interval is "
+                   "discretized. Defaults to 100.")
+        ->check(CLI::Range(1, 1000000));
+    app.add_option("-c,--sec-length", secLength,
+                   "Length of one second in the units of the domain. Defaults to 1000.")
+        ->check(CLI::Range(0.0, 1000000.0));
+    app.add_option("ifx", ifx, "Pixel based x coordinate of arthery input function")->required();
+    app.add_option("ify", ify, "Pixel based y coordinate of arthery input function")->required();
+    app.add_option("ifz", ifz, "Pixel based z coordinate of arthery input function")->required();
+
+    app.add_option("output_folder", outputFolder,
+                   "Folder to which output data after the linear regression.")
+        ->required()
+        ->check(CLI::ExistingDirectory);
+    app.add_option("fitted_coeficients", fittedCoefficients,
+                   "Fourier coeficients fited by the algorithm. Orderred from the first "
+                   "coeficient that corresponds to the constant.")
+        ->required()
+        ->check(CLI::ExistingFile);
+
+    try
+    {
+        app.parse(argc, argv);
+        if(!(startTime < endTime))
+        {
+            io::throwerr("Start time %f must preceed end time %f.", startTime, endTime);
+        }
+        if(secLength == 0.0)
+        {
+            io::throwerr("Length of the second is not positive!");
+        }
+    } catch(const CLI::ParseError& e)
+    {
+        int exitcode = app.exit(e);
+        if(exitcode == 0) // Help message was printed
+        {
+            return 1;
+        } else
+        {
+            LOGE << "Parse error catched";
+            return -1;
+        }
+    }
+    return 0;
+}
+
+int main(int argc, char* argv[])
+{
+    plog::Severity verbosityLevel = plog::debug; // debug, info, ...
+    std::string csvLogFile = io::xprintf(
+        "/tmp/%s.csv", io::getBasename(std::string(argv[0])).c_str()); // Set NULL to disable
+    bool logToConsole = true;
+    plog::PlogSetup plogSetup(verbosityLevel, csvLogFile, logToConsole);
+    plogSetup.initLogging();
+    LOGI << io::xprintf("START %s", argv[0]);
+    Arguments a;
+    int parseResult = a.parseArguments(argc, argv);
+    if(parseResult != 0)
+    {
+        if(parseResult > 0)
+        {
+            return 0; // Exited sucesfully, help message printed
+        } else
+        {
+            return -1; // Exited somehow wrong
+        }
+    }
+    uint16_t dimx, dimy, dimz;
+    io::DenFileInfo di(a.fittedCoefficients[0]);
+    dimx = di.dimx();
+    dimy = di.dimy();
+    dimz = di.dimz();
+    std::shared_ptr<util::Attenuation4DEvaluatorI> concentration
+        = std::make_shared<util::FourierSeriesEvaluator>(
+            a.fittedCoefficients.size(), a.fittedCoefficients, a.startTime, a.endTime);
+    // Vizualization
+    float* convolutionMatrix = new float[a.granularity * a.granularity];
+    float* aif = new float[a.granularity];
+    concentration->timeSeriesIn(a.ifx, a.ify, a.ifz, a.granularity, aif);
+    utils::TikhonovInverse::precomputeConvolutionMatrix(a.granularity, aif, convolutionMatrix);
+#if DEBUG // Ploting AIF
+    util::FourierSeries b(a.fittedCoefficients.size(), a.startTime, a.endTime, 1);
+    b.plotFunctions();
+    std::vector<double> taxis;
+    float* _taxis = new float[a.granularity];
+    concentration->timeDiscretization(a.granularity, _taxis);
+    std::vector<double> plotme;
+    for(uint32_t i = 0; i != a.granularity; i++)
+    {
+        plotme.push_back(aif[i]);
+        taxis.push_back(_taxis[i]);
+    }
+    plt::plot(taxis, plotme);
+    plt::show();
+    delete[] _taxis;
+#endif
+    bool truncatedInstead = false;
+    float lambdaRel = 0.2;
+    utils::TikhonovInverse ti(lambdaRel, truncatedInstead);
+    ti.computePseudoinverse(convolutionMatrix, a.granularity);
+    std::shared_ptr<io::AsyncFrame2DWritterI<float>> ttp_w
+        = std::make_shared<io::DenAsyncFrame2DWritter<float>>(
+            io::xprintf("%s/TTP.den", a.outputFolder.c_str()), dimx, dimy, dimz);
+    std::shared_ptr<io::AsyncFrame2DWritterI<float>> cbf_w
+        = std::make_shared<io::DenAsyncFrame2DWritter<float>>(
+            io::xprintf("%s/CBF.den", a.outputFolder.c_str()), dimx, dimy, dimz);
+    std::shared_ptr<io::AsyncFrame2DWritterI<float>> cbv_w
+        = std::make_shared<io::DenAsyncFrame2DWritter<float>>(
+            io::xprintf("%s/CBV.den", a.outputFolder.c_str()), dimx, dimy, dimz);
+    std::shared_ptr<io::AsyncFrame2DWritterI<float>> mtt_w
+        = std::make_shared<io::DenAsyncFrame2DWritter<float>>(
+            io::xprintf("%s/MTT.den", a.outputFolder.c_str()), dimx, dimy, dimz);
+    util::TimeSeriesDiscretizer tsd(concentration, dimx, dimy, dimz, a.startTime, a.endTime,
+                                    a.secLength, a.threads);
+    LOGD << "TTP computation.";
+    tsd.computeTTP(a.granularity, ttp_w, aif);
+    if(!a.onlyttp)
+    {
+        LOGD << "CBV, CBF and MTT computation.";
+        tsd.computePerfusionParameters(a.granularity, convolutionMatrix, cbf_w, cbv_w, mtt_w);
+    }
+    delete[] convolutionMatrix;
+    delete[] aif;
+    LOGI << io::xprintf("END %s", argv[0]);
+}
